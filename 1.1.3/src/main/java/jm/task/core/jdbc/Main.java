33aa91044ffc1cacd70392792b69fe44854e76f6
tikhonov12 <57409653+tikhonov12@users.noreply.github.com>
2021-07-13 16:59:34 +0500

    Update Main.java

--- 1.1.3/src/main/java/jm/task/core/jdbc/Main.java
+++ 1.1.3/src/main/java/jm/task/core/jdbc/Main.java
@@ -5,17 +5,17 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
+       
         UserServiceImpl service = new UserServiceImpl();
-        service.dropUsersTable();
         service.createUsersTable();
-        service.saveUser("qwe", "sSS", (byte) 12);
-        service.saveUser("qwe", "sSS", (byte) 12);
-        service.saveUser("qwe", "sSS", (byte) 12);
-        service.saveUser("qwe", "sSS", (byte) 12);
-
-        for (User str: service.getAllUsers()){
-            System.out.println(str.getName());
+        service.saveUser("Андрей", "Андреев", (byte) 24);
+        service.saveUser("Владимир", "Владимиров", (byte) 18   );
+        service.saveUser("Стас", "Стасов", (byte) 17);
+        service.saveUser("Анна", "Аннова", (byte) 12);
+        for (User usr: service.getAllUsers()){
+            System.out.println(usr.toString());
         }
         service.cleanUsersTable();
+        service.dropUsersTable();
     }
 }
